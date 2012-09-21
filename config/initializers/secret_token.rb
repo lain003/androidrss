9144c148af9c91e5a45721a5f86bc20f59144b0d
akitani <akitani.r@gmail.com>
2012-09-21 14:04:25 +0900

    AndroidRankingCrowler作成中

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Androidrss::Application.config.secret_token = '8d2953aca94ffc6c701d879b875fe95e0c8cad0e33bd1c98d973d9ffd7e102c299d3638f1a08e5f8721d26f606cab6439ceac6a1ad40fd6bbe3f3b196fdfd04a'
