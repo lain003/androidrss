bd156b28a7bd51c18f30c8c5d2f9cce0c0160389
akitani <akitani.r@gmail.com>
2012-09-27 15:25:22 +0900

    rssをちゃんと作成できるように変更

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -6,10 +6,6 @@ class NewsController < ApplicationController
   def index
     @news = News.all
     
-    result = RSS::Parser.parse("http://b.hatena.ne.jp/hotentry.rss")
-    #result.items.map{ |item| item.link }
-    binding.pry
-    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @news }
--- crontab/ranking_crawler.rb
+++ crontab/ranking_crawler.rb
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 require 'open-uri'
 require 'hpricot'
 require 'pry'
@@ -13,7 +15,6 @@ class RankingCrawler
   def self.create_rss
     rank_in_apps = Hpricot(open("https://play.google.com/store/apps/collection/topselling_free")).search("ul.snippet-list li.goog-inline-block")
     
-    
     rss = REXML::Document.new 
     rss << REXML::XMLDecl.new('1.0', 'UTF-8') 
     rss.add_element("rss", { "version"=>"2.0"})
@@ -21,15 +22,17 @@ class RankingCrawler
     channel = REXML::Element.new("channel")
     channel.add_element("title").add_text "Android Ranking"
     channel.add_element("link").add_text "https://play.google.com/store/apps/collection/topselling_free"
+    channel.add_element("description").add_text "AndroidのRankingのRSS"
     
-    elem = rank_in_apps[0].search("a.title").first
-    item = channel.add_element("item")
-    item.add_element("title").add_text elem["title"]
-    item.add_element("link").add_text elem["href"]
-    item.add_element("description").add_text rank_in_apps[0].search("p.snippet-content").text
+    rank_in_apps.each do |rank_in_app|
+      elem = rank_in_app.search("a.title").first
+      item = channel.add_element("item")
+      item.add_element("title").add_text elem["title"]
+      item.add_element("link").add_text elem["href"]
+      item.add_element("description").add_text rank_in_app.search("p.snippet-content").text
+    end
     
     rss.elements["rss"].add_element(channel)
-    
     
     output_file = File.open("../public/android_rss.xml", "w")
     output_file.write(rss.to_s)
